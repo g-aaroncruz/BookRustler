381e32923d601b29c6ce1aae373177fc73e25ab4
Aaron <g.aaroncruz@gmail.com>
2024-08-06 21:54:04 -0700

    Serializer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use reqwest;
 use std::error::Error;
+use serde::Deserialize;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -9,17 +10,34 @@ struct Args {
     isbn: String,
 }
 
-fn request_isbn(isbn: String) -> Result<(), Box<dyn Error>> {
-    println!("{}", isbn);
+#[derive(Deserialize, Debug)]
+struct Book {
+    title: Option<String>,
+    authors: Option<Vec<Author>>,
+    publish_date: Option<String>,
+    publishers: Option<Vec<String>>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Author {
+    key: Option<String>,
+    name: Option<String>,
+}
+
+fn request_isbn(isbn: String) -> Result<Book, Box<dyn Error>> {
     let body = reqwest::blocking::get(&format!("https://openlibrary.org/isbn/{isbn}.json"))?
         .text()?;
-    println!("body = {body:?}");
-    Ok(())
+    let json: Book = serde_json::from_str(&body)?;
+    Ok(json)
 }
 fn main() {
     let args = Args::parse();
     assert_eq!(args.isbn.is_empty(), false);
-    if let Err(e) = request_isbn(args.isbn) {
-        eprintln!("Error: {}", e);
+    match request_isbn(args.isbn) {
+        Ok(json) => {
+            println!("JSON: {:?}", json);
+        }
+        Err(e) => println!("Error: {}", e),
     }
+
 }
